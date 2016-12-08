85179d2d9498735c22b7240a6b050928ec035a8d
zjx <zhao_j1@denison.edu>
2016-12-08 17:54:58 -0600

    done

--- PriceGenerater.py
+++ PriceGenerater.py
@@ -3,10 +3,9 @@ class PriceGenerater(object):
     def __init__(self):
         return
 
-    def generatePriceBasic(self, maxPrice, minPrice):
-        out = []
+    def generatePriceBasic(self,maxCapacity, maxPrice, minPrice):
+        out = {} 
         for i in xrange(365):
-            out.append((i, random.randint(maxPrice, minPrice)))
         return out
 
             
